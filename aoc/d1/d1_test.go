8f6db64c68862a608ff823043a5977ea71be8cbd
Lucas Rijllart <lucasrijllart@Lucas-M2-Air.local>
2022-12-18 19:59:30 +0000

    d1p2 done

--- aoc/d1/d1_test.go
+++ aoc/d1/d1_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestExampleInput(t *testing.T) {
+func TestD1(t *testing.T) {
 	tests := []struct {
 		inputs   string
 		expected int
@@ -19,6 +19,29 @@ func TestExampleInput(t *testing.T) {
 
 	for _, test := range tests {
 		result := find_max_cal(test.inputs)
+		if result != test.expected {
+			t.Errorf("Got %d, expected %d", result, test.expected)
+		}
+	}
+}
+
+func TestD2(t *testing.T) {
+	tests := []struct {
+		inputs   string
+		expected int
+	}{
+		{
+			inputs:   "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000",
+			expected: 45000,
+		},
+		{
+			inputs:   "1000\n\n2000\n\n3000\n\n4000",
+			expected: 9000,
+		},
+	}
+
+	for _, test := range tests {
+		result := find_top_three(test.inputs)
 		if result != test.expected {
 			t.Errorf("Got %d, expected %d", result, test.expected)
 		}
---
+++ aoc/d1/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func total_calories_by_elf(input []string) map[int]int {
+	elves := make(map[int]int)
+	var elf_id int
+	elf_id = 1
+	total_calories := 0
+	for _, calories_str := range input {
+		//fmt.Println(calories_str)
+		calories, _ := strconv.Atoi(calories_str)
+		total_calories = total_calories + calories
+		if calories_str == "" {
+			elves[elf_id] = total_calories
+			elf_id++
+			total_calories = 0
+		}
+	}
+	elves[elf_id] = total_calories
+	return elves
+}
+
+func find_max_cal(input string) int {
+	list_of_calories := strings.Split(input, "\n")
+	elves := total_calories_by_elf(list_of_calories)
+
+	max_cal := 0
+	for _, calories := range elves {
+		if calories > max_cal {
+			max_cal = calories
+		}
+	}
+	return max_cal
+}
+
+func find_top_three(input string) int {
+	list_of_calories := strings.Split(input, "\n")
+	elves_map := total_calories_by_elf(list_of_calories)
+	elves := make([]int, 0, len(elves_map))
+	for _, value := range elves_map {
+		elves = append(elves, value)
+	}
+
+	sort.Ints(elves)
+	top_three := elves[len(elves)-3:]
+	fmt.Println(top_three)
+	total := (top_three[0] + top_three[1] + top_three[2])
+	return total
+}
+
+func read_file(file_path string) string {
+	file, _ := os.ReadFile(file_path)
+	return string(file)
+}
+
+func d1() {
+	// file_path := "aoc/d1/test1.txt"
+	file_path := "aoc/submission.txt"
+	// fmt.Println(string(file))
+	max_cal := find_max_cal(read_file(file_path))
+	fmt.Println(max_cal)
+}
+
+func d2() {
+	file_path := "aoc/submission.txt"
+	top_three := find_top_three((read_file(file_path)))
+	fmt.Println(top_three)
+}
+
+func main() {
+	function_name := os.Args[1]
+	fmt.Println(function_name)
+	funcs := map[string]func(){
+		"d1": d1,
+		"d2": d2,
+	}
+	funcs[function_name]()
+}
