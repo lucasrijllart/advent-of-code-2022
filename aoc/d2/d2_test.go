2fff5341d59af91bfbffe6506d766a2566820ee9
Lucas Rijllart <lucasrijllart@Lucas-M2-Air.local>
2022-12-31 09:47:23 +0000

    d2p2 complete

--- aoc/d2/d2_test.go
+++ aoc/d2/d2_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestD2(t *testing.T) {
+func TestD2P1(t *testing.T) {
 	tests := []struct {
 		inputs   string
 		expected int
@@ -14,7 +14,7 @@ func TestD2(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		result := RPSWin(test.inputs)
+		result := GoThroughFile(test.inputs, ScoreFromOneGame)
 		if result != test.expected {
 			t.Errorf("Got %d, expected %d", result, test.expected)
 		}
@@ -34,6 +34,44 @@ func TestScoreFromOneGame(t *testing.T) {
 
 	for _, test := range tests {
 		result := ScoreFromOneGame(test.move, test.response)
+		if result != test.expected {
+			t.Errorf("Got %d, expected %d", result, test.expected)
+		}
+	}
+}
+
+func TestD2P2(t *testing.T) {
+	tests := []struct {
+		inputs   string
+		expected int
+	}{
+		{
+			inputs:   "A Y\nB X\nC Z",
+			expected: 12,
+		},
+	}
+
+	for _, test := range tests {
+		result := GoThroughFile(test.inputs, ScoreFromRoundEnd)
+		if result != test.expected {
+			t.Errorf("Got %d, expected %d", result, test.expected)
+		}
+	}
+}
+
+func TestRoundEnd(t *testing.T) {
+	tests := []struct {
+		move     string
+		response string
+		expected int
+	}{
+		{move: "A", response: "Y", expected: 4},
+		{move: "B", response: "X", expected: 1},
+		{move: "C", response: "Z", expected: 7},
+	}
+
+	for _, test := range tests {
+		result := ScoreFromRoundEnd(test.move, test.response)
 		if result != test.expected {
 			t.Errorf("Got %d, expected %d", result, test.expected)
 		}
--- aoc/d2/main.go
+++ aoc/d2/main.go
@@ -6,6 +6,18 @@ import (
 	"strings"
 )
 
+// Returns the points from doing a shape
+// Used by both ScoreFromOneGame and ScoreFromRoundEnd
+func ShapePoints(shape string) int {
+	return map[string]int{
+		"X": 1, // rock
+		"Y": 2, // paper
+		"Z": 3, // scissors
+	}[shape]
+}
+
+// Returns the score from the shape played by the opponent and by us
+// Used by p1
 func ScoreFromOneGame(move string, response string) int {
 	game_win_map := map[string]int{
 		"A X": 3, // rock - rock = draw
@@ -18,27 +30,53 @@ func ScoreFromOneGame(move string, response string) int {
 		"C Y": 0, // scissors - paper = lose
 		"C Z": 3, // scissors - scissors = draw
 	}
-	move_points_map := map[string]int{
-		"X": 1, // rock
-		"Y": 2, // paper
-		"Z": 3, // scissors
-	}
-
+	move_points := ShapePoints(response)
 	game_points := game_win_map[move+" "+response]
-	move_points := move_points_map[response]
 	return game_points + move_points
 }
 
-func RPSWin(input string) int {
+// From a move and a game outcome, return the shape that needs to be played
+func GetResponseFromGameEnd(move string, game_should_end string) string {
+	return map[string]string{
+		"A X": "Z", // rock - lose = scissors
+		"A Y": "X", // rock - draw = rock
+		"A Z": "Y", // rock - win = paper
+		"B X": "X", // paper - lose = rock
+		"B Y": "Y", // paper - draw = paper
+		"B Z": "Z", // paper - win = scissors
+		"C X": "Y", // scissors - lose = paper
+		"C Y": "Z", // scissors - draw = scissors
+		"C Z": "X", // scissors - win = rock
+	}[move+" "+game_should_end]
+}
+
+// Returns the score from a shape by the opponent and a game outcome
+func ScoreFromRoundEnd(move string, game_should_end string) int {
+	game_end_map := map[string]int{
+		"X": 0, // lose
+		"Y": 3, // draw
+		"Z": 6, // win
+	}
+	game_points := game_end_map[game_should_end]
+	response_shape := GetResponseFromGameEnd(move, game_should_end)
+	move_points := ShapePoints(response_shape)
+	return game_points + move_points
+}
+
+type part_function func(string, string) int
+
+// Goes through the file and runs the desired function for each line then adds up total points
+func GoThroughFile(input string, function part_function) int {
 	games := strings.Split(input, "\n")
 	total_score := 0
 	for _, game := range games {
 		moves := strings.Split(game, " ")
 		move := moves[0]
 		response := moves[1]
-		score := ScoreFromOneGame(move, response)
+		score := function(move, response)
 		total_score = total_score + score
-		//fmt.Println("move", move, "response", response, "score", score, "total", total_score)
+		fmt.Println("move", move, "response", response, "score",
+			score, "total", total_score)
 	}
 	return total_score
 }
@@ -47,7 +85,15 @@ func p1() {
 	//file_path := "aoc/d2/test.txt"
 	file_path := "aoc/d2/submission.txt"
 	file_string, _ := os.ReadFile(file_path)
-	total := RPSWin(string(file_string))
+	total := GoThroughFile(string(file_string), ScoreFromOneGame)
+	fmt.Println(total)
+}
+
+func p2() {
+	//file_path := "aoc/d2/test.txt"
+	file_path := "aoc/d2/submission.txt"
+	file_string, _ := os.ReadFile(file_path)
+	total := GoThroughFile(string(file_string), ScoreFromRoundEnd)
 	fmt.Println(total)
 }
 
@@ -56,6 +102,7 @@ func main() {
 	fmt.Println(function_name)
 	funcs := map[string]func(){
 		"p1": p1,
+		"p2": p2,
 	}
 	funcs[function_name]()
 }
