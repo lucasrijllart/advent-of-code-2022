a1d90955e1459303babcbd8f9e0c500b4c0a36bd
Lucas Rijllart <lucasrijllart@Lucas-M2-Air.local>
2023-04-12 19:53:18 -0500

    move containers works

--- aoc/d5/d5_test.go
+++ aoc/d5/d5_test.go
@@ -11,23 +11,39 @@ import (
 func TestParseInput(t *testing.T) {
 	file, _ := os.ReadFile("test.txt")
 	containers, moves := parseInput(string(file))
-	expected_containers := [][]string{
+	expectedContainers := [][]string{
 		{"Z", "N"},
 		{"M", "C", "D"},
 		{"P"},
 	}
-	expected_moves := [][]int{
+	expectedMoves := [][]int{
 		{1, 2, 1},
 		{3, 1, 3},
 		{2, 2, 1},
 		{1, 1, 2},
 	}
 
-	if reflect.DeepEqual(containers, expected_containers) != true {
-		t.Errorf("Containers are not the same: %v, expected %v", containers, expected_containers)
+	if reflect.DeepEqual(containers, expectedContainers) != true {
+		t.Errorf("Containers are not the same: %v, expected %v", containers, expectedContainers)
 	}
 
-	if reflect.DeepEqual(moves, expected_moves) != true {
-		t.Errorf("Moves are not the same: %v, expected %v", moves, expected_moves)
+	if reflect.DeepEqual(moves, expectedMoves) != true {
+		t.Errorf("Moves are not the same: %v, expected %v", moves, expectedMoves)
+	}
+}
+
+// ensure that the function runs the moves on the stacks of containers correctly
+func TestMoveContainers(t *testing.T) {
+	file, _ := os.ReadFile("test.txt")
+	containers, moves := parseInput(string(file))
+	movedContainers := MoveContainers(containers, moves)
+	expectedContainers := [][]string{
+		{"C"},
+		{"M"},
+		{"P", "D", "N", "Z"},
+	}
+
+	if reflect.DeepEqual(movedContainers, expectedContainers) != true {
+		t.Errorf("Containers are not the same: %v, expected %v", movedContainers, expectedContainers)
 	}
 }
--- aoc/d5/main.go
+++ aoc/d5/main.go
@@ -29,7 +29,8 @@ func p1() {
 	file_path := "aoc/d5/test.txt"
 	// file_path := "aoc/d4/submission.txt"
 	file, _ := os.ReadFile(file_path)
-	containers, _ := parseInput(string(file))
+	containers, moves := parseInput(string(file))
+	containers = MoveContainers(containers, moves)
 	fmt.Println(containers)
 }
 
@@ -50,12 +51,11 @@ func p2() {
 
 func parseInput(file_input string) ([][]string, [][]int) {
 	parts := strings.Split(file_input, "\n\n")
+	fmt.Println(parts[1])
 	containersList, movesList := strings.Split(parts[0], "\n"), strings.Split(parts[1], "\n")
 
 	var containers [][]string
 	var moves [][]int
-
-	//fmt.Println(containersStr)
 
 	// looping through every 4th number starting with 1 so 1,5,9,... as that's the string index of where the stack is
 	for stackIndex := 1; stackIndex < len(containersList[0]); stackIndex = stackIndex + 4 {
@@ -83,4 +83,18 @@ func parseInput(file_input string) ([][]string, [][]int) {
 	}
 
 	return containers, moves
+}
+
+func MoveContainers(containers [][]string, moves [][]int) [][]string {
+	for _, move := range moves {
+		containersToMove, from, to := move[0], move[1]-1, move[2]-1 // add offset to from
+		for moving := 0; moving < containersToMove; moving++ {
+			container := containers[from][len(containers[from])-1]        // get value
+			containers[from] = containers[from][:len(containers[from])-1] // pop value
+			containers[to] = append(containers[to], container)
+			fmt.Printf("Moving %v from %v to %v \n", container, from, to)
+		}
+		fmt.Printf("%v\n", move)
+	}
+	return containers
 }
